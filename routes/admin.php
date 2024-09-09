6410bea23653aff20a954d6e1796730e769bee96
Szaniszló Árpád (MArpadszF) <developedbybarley@gmail.com>
2024-09-09 14:27:04 +0200

    Admin middleware

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\AdminAuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('admin')->group(function () {
-    Route::get('/',[ AdminController::class, 'showLoginPage'])->name('login');
-    Route::get('/dashboard', [AdminController::class , 'dashboard'])->middleware('auth:admin')->name('admin.dashboard');
+    Route::get('/', [AdminController::class, 'showLoginPage'])->name('login');
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->middleware(AdminAuthMiddleware::class)->name('admin.dashboard');
     Route::post('/', [AdminController::class, 'login'])->name('admin.login');
 });
